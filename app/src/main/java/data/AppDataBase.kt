2e655ca88cfdcedfd45cf2eaefd8de598918f2df
Yoel <yobelb8@gmail.com>
2024-11-20 09:44:27 +0000

    Creada base de datos (No adaptada a TipoTarea)

--- app/src/main/java/data/AppDataBase.kt
+++ app/src/main/java/data/AppDataBase.kt
@@ -1,4 +1,29 @@
 package data
 
-class AppDataBase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import data.Task
+import data.TaskDao
+
+@Database(entities = [Task::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun taskDao(): TaskDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "task_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
 }
