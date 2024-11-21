3babb704830fea12d493db1f453b78ca3b3f4737
Yoel <yobelb8@gmail.com>
2024-11-21 12:32:58 +0000

    Cambio de base de datos por errores de la anterior

--- app/src/main/java/data/AppDataBase.kt
+++ app/src/main/java/data/AppDataBase.kt
@@ -18,7 +18,7 @@ abstract class AppDatabase : RoomDatabase() {
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
                     AppDatabase::class.java,
-                    "task_database"
+                    "tasks_database"
                 ).build()
                 INSTANCE = instance
                 instance
