e5e8f31b0824b7fb821fbdb292777b8f03cc08b4
Yoel <yobelb8@gmail.com>
2024-11-21 10:02:47 +0000

    Inicializada primera versión (Sin TipoTareas)

--- app/src/main/java/com/yoel/listadetareas/MainActivity.kt
+++ app/src/main/java/com/yoel/listadetareas/MainActivity.kt
@@ -1,20 +1,21 @@
-package com.yoel.composeroomapp
+package com.yoel.listadetareas
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import data.AppDatabase
 import view.*
 
 class MainActivity : ComponentActivity() {
-    //private lateinit var database: AppDatabase
+    private lateinit var database: AppDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // Inicializar la base de datos
-        // database = AppDatabase.getDatabase(this)
+        database = AppDatabase.getDatabase(this)
         setContent {
             //TaskApp(database)
-            TaskApp()
+            TaskApp(database)
         }
     }
 }
