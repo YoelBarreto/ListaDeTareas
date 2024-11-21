b7e4e913fd170f12584089434fd4a85c4bd6d53f
Yoel <yobelb8@gmail.com>
2024-11-21 11:41:14 +0000

    Creación de uso de la relación de Task y TypeTask

---
+++ app/src/main/java/data/TaskWithTypeTask.kt
@@ -0,0 +1,13 @@
+package data
+
+import androidx.room.Embedded
+import androidx.room.Relation
+
+data class TaskWithTypeTask(
+    @Embedded val task: Task,
+    @Relation(
+        parentColumn = "typeTaskId",
+        entityColumn = "id"
+    )
+    val typeTask: TypeTask
+)
