6901fd84895a46732896eec231f9f576e8b6135b
Yoel <yobelb8@gmail.com>
2024-11-27 08:43:11 +0000

    Eliminada librerias no usadas + comentarios

--- app/src/main/java/view/TaskApp.kt
+++ app/src/main/java/view/TaskApp.kt
@@ -14,13 +14,11 @@ import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -33,7 +31,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 
-@OptIn(ExperimentalMaterial3Api::class)
+
 @Composable
 fun TaskApp(database: AppDatabase) {
     val taskDao = database.taskDao()
@@ -344,6 +342,7 @@ fun TaskApp(database: AppDatabase) {
                                                  TextButton(
                                                      onClick = {
                                                          try {
+                                                             // Actualizar tarea
                                                              scope.launch(Dispatchers.IO) {
                                                                  taskDao.updateTask(
                                                                      Task(
