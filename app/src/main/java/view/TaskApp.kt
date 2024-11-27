cd4b9b016bc850aa331b690f52acea325254de54
Yoel <yobelb8@gmail.com>
2024-11-27 09:02:39 +0000

    Añadida pantalla de edición de tipo de tarea

--- app/src/main/java/view/TaskApp.kt
+++ app/src/main/java/view/TaskApp.kt
@@ -31,6 +31,164 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 
+@Composable
+fun TypeTaskApp(database: AppDatabase) {
+    val typeTaskDao = database.typeTaskDao()
+    val scope = rememberCoroutineScope()
+    var typeTasks by remember { mutableStateOf(listOf<TypeTask>()) }
+
+    // Tipo tareas
+    var newTypeTaskName by remember { mutableStateOf("") }
+
+    // Cargar tareas al iniciar
+    LaunchedEffect(Unit) {
+        try {
+            typeTasks = typeTaskDao.getAllTypeTasks()
+        } catch (e: Exception) {
+            Log.i("Error", e.toString())
+        }
+    }
+
+    Column(
+        modifier = Modifier
+            .background(Color(0xFFEAA9FF))
+            .fillMaxSize()
+            .padding(15.dp)
+            .verticalScroll(state = rememberScrollState()),
+    ) {
+        Row {
+            OutlinedTextField(
+                value = newTypeTaskName,
+                onValueChange = { newTypeTaskName = it },
+                label = {
+                    Text(
+                        text = "Tipo tarea",
+                        style = TextStyle(
+                            fontWeight = FontWeight.Bold
+                        ),)
+
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(end = 3.dp)
+//                    .background(Color(0xFFF1C7FF))
+            )
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 20.dp, bottom = 20.dp),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Button(
+                modifier = Modifier
+                    .width(170.dp)
+                    .height(50.dp),
+                onClick = {
+                    // Corrutina para añadir tarea
+                    scope.launch(Dispatchers.IO) {
+                        try {
+                            typeTaskDao.insertTypeTask(
+                                TypeTask(
+                                    titulo = newTypeTaskName,
+                                )
+                            )
+                            // Limpiar los cambios de input
+                            newTypeTaskName = ""
+
+                            // Refresca las lista de tareas
+                            typeTasks = typeTaskDao.getAllTypeTasks()
+                        } catch (e: Exception) {
+                            Log.i("Error", e.toString())
+                        }
+                    }
+                }
+            ) {
+                Text("Añadir Tarea")
+            }
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 20.dp, bottom = 20.dp),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Text(
+                    text = "Lista de Tareas",
+                    modifier = Modifier.padding(bottom = 35.dp),
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 35.sp
+                )
+                Column {
+                    // Mostrar la lista de tareas
+                    typeTasks.forEach { typeTask ->
+                        // Tarjeta de tarea
+                        Box(
+                            modifier = Modifier
+                                .padding(bottom = 10.dp)
+                                .clip(shape = RoundedCornerShape(10.dp))
+                                .background(Color(0xFFF1C7FF))
+                                .height(75.dp)
+                        ) {
+                            Row(
+                                modifier = Modifier.fillMaxSize(),
+                                horizontalArrangement = Arrangement.Center,
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                Column(
+                                    modifier = Modifier
+                                        .weight(0.35f)
+                                        .padding(start = 5.dp, top = 10.dp)
+                                ) {
+                                    Text(
+                                        text = typeTask.titulo,
+                                        fontSize = 18.sp,
+                                        fontWeight = FontWeight.Bold
+                                    )
+                                }
+                                Row(
+                                    modifier = Modifier
+                                        .weight(0.4f)
+                                        .padding(end = 5.dp),
+                                    horizontalArrangement = Arrangement.Center
+                                ) {
+                                    androidx.compose.material.IconButton(
+                                        modifier = Modifier,
+                                        onClick = {
+                                        }
+                                    ) {
+                                        Icon(Icons.Default.Edit, contentDescription = "Editar")
+                                    }
+                                    androidx.compose.material.IconButton(
+                                        modifier = Modifier,
+                                        onClick = {
+                                            scope.launch(Dispatchers.IO) {
+                                                try {
+                                                    typeTaskDao.deleteTypeTask(typeTask)
+
+                                                    typeTasks = typeTaskDao.getAllTypeTasks()
+                                                } catch (e: Exception) {
+                                                    Log.i("Error", e.toString())
+
+                                                }
+                                            }
+                                        }
+                                    ) {
+                                        Icon(Icons.Default.Delete, contentDescription = "Eliminar")
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
 
 @Composable
 fun TaskApp(database: AppDatabase) {
@@ -146,6 +304,7 @@ fun TaskApp(database: AppDatabase) {
 
             Button(
                 onClick = {
+                    TypeTaskApp()
                 },
                 modifier = Modifier
                     .padding(start = 5.dp, top = 5.dp)
